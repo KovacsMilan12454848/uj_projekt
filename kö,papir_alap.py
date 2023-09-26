d0f6f781b9a9d7ddfff9a7cec83b5af4e7c5b757
KovacsMilan12454848 <144911254+KovacsMilan12454848@users.noreply.github.com>
2023-09-26 09:35:33 +0200

    Kő,papir alap

---
+++ kö,papir_alap.py
@@ -0,0 +1,6 @@
+szam1=str(input('kő, papir vagy olló válasz(1):'))
+szam2=str(input('kő, papir vagy olló válasz(2):'))
+
+
+if szam1=='kő' and szam2=='kő':
+    print('mind a ketten követ választotatok így ez döntetlen')
