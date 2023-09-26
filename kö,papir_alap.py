b305234e367d98f6ab93066533688fc9db69902d
faragomartin2007 <144911492+faragomartin2007@users.noreply.github.com>
2023-09-26 09:51:13 +0200

    Update kö,papir_alap.py

--- kö,papir_alap.py
+++ kö,papir_alap.py
@@ -20,3 +20,6 @@ if szam1=='olló' and szam2=='papir' or szam1=='papir' and szam2=='olló':
 #kŐ olló
 if szam1=='kő' and szam2=='olló' or szam1=='olló' and szam2=='kő':
     print("A kő nyert!")
+#kő papír
+if szam1=="kő " and szam2=="papir" or szam1=='papir' and szam2=="kő":
+    print("A papír nyert!")
