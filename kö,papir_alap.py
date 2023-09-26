ed2d556682e3f5ba3631a2c74f5bab5724bbcf55
KovacsMilan12454848 <144911254+KovacsMilan12454848@users.noreply.github.com>
2023-09-26 09:43:49 +0200

    Update kö,papir_alap.py

--- kö,papir_alap.py
+++ kö,papir_alap.py
@@ -1,6 +1,21 @@
 szam1=str(input('kő, papir vagy olló válasz(1):'))
 szam2=str(input('kő, papir vagy olló válasz(2):'))
 
-
+#kő
 if szam1=='kő' and szam2=='kő':
     print('mind a ketten követ választotatok így ez döntetlen')
+    
+#papir    
+if szam1=='papir' and szam2=='papir':
+    print('mind a ketten papirt választotatok így ez döntetlen')
+    
+#olló    
+if szam1=='olló' and szam2=='olló':
+    print('mind a ketten ollót választotatok így ez döntetlen')
+    
+    
+#olló papir    
+if szam1=='olló' and szam2=='papir' or szam1=='papir' and szam2=='olló':
+    print('Az olló nyert')
+
+
